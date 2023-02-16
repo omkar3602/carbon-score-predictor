2ec51e84a4352c4ab3624065733e07cd20bc0c44
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-02-16 18:53:02 +0530

    Add admin role based access

--- mainapp/views.py
+++ mainapp/views.py
@@ -13,9 +13,13 @@ def index(request):
 @login_required_message(message="Please log in, in order to view the requested page.")
 @login_required
 def oxygen_emission(request):
+    if request.user.is_admin:
+            return redirect('adminuser')
     return render(request, 'mainapp/get_oxygen_emission.html')
 
 @login_required_message(message="Please log in, in order to view the requested page.")
 @login_required
 def carbon_footprint(request):
+    if request.user.is_admin:
+            return redirect('adminuser')
     return render(request, 'mainapp/get_carbon_footprint.html')
