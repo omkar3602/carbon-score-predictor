869f03baa89f808eb6cdf53986ec9476c814b467
neharaut00 <neharaut372@gmail.com>
2023-02-16 17:07:57 +0530

    signup bugs

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,12 +5,13 @@ from django.utils import timezone
 # Create your models here.
 class Oxygen_Emission(models.Model):
     plant_species = models.CharField(max_length=30)
-    light_intensity = models.DecimalField(..., max_digits=5, decimal_places=2)
-    carbon_emission = models.DecimalField(..., max_digits=5, decimal_places=2)
-    temperature = models.DecimalField(..., max_digits=5, decimal_places=2)
+    light_intensity = models.DecimalField(max_digits=5, decimal_places=2)
+    carbon_emission = models.DecimalField(max_digits=5, decimal_places=2)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
 
     submitted_on = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
 
+    oxygen_emission = models.DecimalField(max_digits=5, decimal_places=2)
     def __str__(self):
         return self.user + "-" + self.submitted_on
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from utils.decorator import login_required_message
 
 # Create your views here.
 def index(request):
@@ -8,8 +10,12 @@ def index(request):
         return render(request, 'mainapp/home.html')
     return render(request, 'mainapp/index.html')
 
+@login_required_message(message="Please log in, in order to view the requested page.")
+@login_required
 def oxygen_emission(request):
     return render(request, 'mainapp/get_oxygen_emission.html')
 
+@login_required_message(message="Please log in, in order to view the requested page.")
+@login_required
 def carbon_footprint(request):
     return render(request, 'mainapp/get_carbon_footprint.html')
