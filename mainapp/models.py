38ffe1c04ac9a6b2b40f7b66f4448aa42fd2725d
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-02-16 16:28:15 +0530

    Add oxygen emission

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from userauth.models import Account
+from django.utils import timezone
 
 # Create your models here.
+class Oxygen_Emission(models.Model):
+    plant_species = models.CharField(max_length=30)
+    light_intensity = models.DecimalField(..., max_digits=5, decimal_places=2)
+    carbon_emission = models.DecimalField(..., max_digits=5, decimal_places=2)
+    temperature = models.DecimalField(..., max_digits=5, decimal_places=2)
+
+    submitted_on = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user + "-" + self.submitted_on
