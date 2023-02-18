ce918321aa00d8217eadcb2cd6728a9a743b7c7e
omkar3602 <73201152+omkar3602@users.noreply.github.com>
2023-02-18 15:32:17 +0530

    Round carbon score

--- utils/score_generator.py
+++ utils/score_generator.py
@@ -12,9 +12,9 @@ def get_carbon_score(oxygen_val, home_appliance_co2_val, vehicle_co2_val, waste_
 
         load_dotenv()
         if net_carbon_emission < 0:
-                send_mail(user.email, f"Check your Carbon score.", f"Hey {user.fullname}. Congratulations!! Your carbon score is net-negative ({net_carbon_emission}). Check it out here {os.getenv('WEB_URL')}#carbon_score")
+                send_mail(user.email, f"Check your Carbon score.", f"Hey {user.fullname}. Congratulations!! Your carbon score is net-negative ({round(net_carbon_emission, 2)}). Check it out here {os.getenv('WEB_URL')}#carbon_score")
         else:
-                send_mail(user.email, f"Check your Carbon score.", f"Hey {user.fullname}. You need to work on reducing your carbon footprint. Your carbon score is net-positive ({net_carbon_emission}). Check it out here {os.getenv('WEB_URL')}#carbon_score")
+                send_mail(user.email, f"Check your Carbon score.", f"Hey {user.fullname}. You need to work on reducing your carbon footprint. Your carbon score is net-positive ({round(net_carbon_emission, 2)}). Check it out here {os.getenv('WEB_URL')}#carbon_score")
 
 
         carbon_score_model.save()
