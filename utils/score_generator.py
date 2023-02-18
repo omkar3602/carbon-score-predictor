9354ff7d38987481c97d5df7bf3f20d3df9948c6
Adwait Bhosale <adwaitbhosale04@gmail.com>
2023-02-18 11:29:10 +0530

    Float val

--- utils/score_generator.py
+++ utils/score_generator.py
@@ -6,7 +6,7 @@ import os
 def get_carbon_score(oxygen_val, home_appliance_co2_val, vehicle_co2_val, waste_management_co2_val, user):
         mean_co2_emission = (home_appliance_co2_val+vehicle_co2_val+waste_management_co2_val)/3
         
-        net_carbon_emission = mean_co2_emission - oxygen_val
+        net_carbon_emission = float(mean_co2_emission) - float(oxygen_val)
         
         carbon_score_model = Carbon_Score(carbon_score=net_carbon_emission, user=user)
 
